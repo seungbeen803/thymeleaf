a4cd454267cfc1a90226ef01feb7b5d0c4b7bf51
seungbeen <seungbin803@gmail.com>
2023-05-22 12:11:58 +0900

    Add: template3

--- src/main/java/study/thymeleaf/MyController.java
+++ src/main/java/study/thymeleaf/MyController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class MyController {
     @GetMapping("/template1")
@@ -42,4 +47,30 @@ public class MyController {
         // ModelAndView 객체 반환
         return mav;
     }
+
+    @GetMapping("/template3")
+    // 템플릿 엔진에서 그려야할 내용을 template3에 전달해주는 역할을 한다
+    public String template3(Model m) {
+        m.addAttribute("condition1", true);
+        m.addAttribute("condition2", false);
+        Book book = new Book("김철수", "부자되는 방법", 10000);
+        book.setSale(true);
+        book.setSaleAmount(0.3);
+        m.addAttribute("book", book);
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("김철수", "부자되는 방법 1", 10000));
+        bookList.add(new Book("김철수", "부자되는 방법 2", 20000, true, 0.2));
+        bookList.add(new Book("김철수", "부자되는 방법 3", 30000));
+        bookList.add(new Book("김철수", "부자되는 방법 4", 40000, true, 0.3));
+        m.addAttribute("book_list", bookList);
+        Map<String, Book> bookMap = new HashMap<>();
+        bookMap.put("book1", new Book("김철수", "부자되는 방법 1", 10000));
+        bookMap.put("book2", new Book("김철수", "부자되는 방법 2", 20000, true, 0.2));
+        bookMap.put("book3", new Book("김철수", "부자되는 방법 3", 30000));
+        bookMap.put("book4", new Book("김철수", "부자되는 방법 4", 40000, true, 0.3));
+        m.addAttribute("book_map", bookMap);
+        m.addAttribute("grade", "A");
+        return "template3";
+    }
+
 }
